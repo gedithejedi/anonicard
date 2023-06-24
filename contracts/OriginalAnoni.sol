bd92ede7358e5c3638e13e8b3a2f044fdafd1302
“DaeunYoon” <daa3230@gmail.com>
2023-06-23 22:10:25 -0400

    contracts scaffold

---
+++ contracts/OriginalAnoni.sol
@@ -0,0 +1,28 @@
+//SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+
+contract OriginalAnoni is ERC721URIStorage {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+
+    constructor() ERC721("OriginalAnoni", "ORIGINALANONI") {}
+
+    function mint(string memory tokenURI) public returns (uint256) {
+        _tokenIds.increment();
+
+        uint256 newItemId = _tokenIds.current();
+        _mint(msg.sender, newItemId);
+        _setTokenURI(newItemId, tokenURI);
+
+        return newItemId;
+    }
+
+
+    function totalSupply() public view returns (uint256) {
+        return _tokenIds.current();
+    }
+}
