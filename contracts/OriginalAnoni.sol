f069241be88e732ee493b3e557af1223d172f36f
“DaeunYoon” <daa3230@gmail.com>
2023-06-23 22:24:24 -0400

    scaffold nft

--- contracts/OriginalAnoni.sol
+++ contracts/OriginalAnoni.sol
@@ -25,4 +25,6 @@ contract OriginalAnoni is ERC721URIStorage {
     function totalSupply() public view returns (uint256) {
         return _tokenIds.current();
     }
+
+    // TODO: update function - update is available & only by owner
 }
