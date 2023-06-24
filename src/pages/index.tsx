5d1e43857efd318dbb94186a1551cc02a9a0c75e
“DaeunYoon” <daa3230@gmail.com>
2023-06-24 10:21:50 -0400

    install worldcoin

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,13 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useCallback } from 'react'
+import dynamic from 'next/dynamic'
 
 import alchemy from '~/alchemy'
 import { useAccount } from 'wagmi'
+import { IDKitWidget } from '@worldcoin/idkit'
+import type { ISuccessResult } from '@worldcoin/idkit'
 
 import Box from '~/components/Common/Box'
+import Button from '~/components/Common/Button'
 import OriginalForm from '~/components/OriginalForm'
 import useUserOwnedNfts from '~/hooks/useUserOwnedNfts'
 
@@ -15,6 +19,13 @@ interface OriginalNFT {
   job: number
   introduction: string
 }
+
+// const IDKitWidget = dynamic(
+//   () => import('@worldcoin/idkit').then((mod) => mod.IDKitWidget),
+//   { ssr: false }
+// )
+
+const WORLDCOIN_ID = process.env.NEXT_PUBLIC_WORLDCOIN_ID
 
 export default function Home() {
   const { address } = useAccount()
@@ -28,6 +39,16 @@ export default function Home() {
   useEffect(() => {
     getUserOwnedNfts()
   }, [])
+
+  const handleProof = useCallback((result: ISuccessResult) => {
+    return new Promise<void>((resolve) => {
+      setTimeout(() => resolve(), 3000)
+    })
+  }, [])
+
+  const onSuccess = (result: ISuccessResult) => {
+    console.log(result)
+  }
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-4">
@@ -60,7 +81,14 @@ export default function Home() {
             </table>
           </>
         ) : (
-          <OriginalForm />
+          <IDKitWidget
+            action="verifyhuman"
+            onSuccess={(result) => console.log(result)}
+            handleVerify={handleProof}
+            app_id={WORLDCOIN_ID} // obtain this from developer.worldcoin.org
+          >
+            {({ open }) => <Button onClick={open}>Click me</Button>}
+          </IDKitWidget>
         )}
       </Box>
     </main>
