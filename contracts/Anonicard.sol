91f3672f26345d56e19f1bfc8e093345ca021dd3
“DaeunYoon” <daa3230@gmail.com>
2023-06-25 04:52:01 -0400

    update anonicard contract

--- contracts/Anonicard.sol
+++ contracts/Anonicard.sol
@@ -9,37 +9,25 @@ contract AnoniCard is ERC721URIStorage {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
 
-    struct TokenMetadata {
-        string cardInfoURI;
-        string customFieldsURI;
-    }
-
-    mapping(uint256 => TokenMetadata) private _tokenMetadata;
-
     constructor() ERC721("AnoniCard", "ANONICARD") {}
 
     function mint(
         address receiver,
-        string memory cardInfoURI,
-        string memory customFieldsURI
+        string memory tokenURI
     ) public returns (uint256) {
         _tokenIds.increment();
 
         uint256 newItemId = _tokenIds.current();
         _mint(receiver, newItemId);
-
-        _tokenMetadata[newItemId] = TokenMetadata(
-            cardInfoURI,
-            customFieldsURI
-        );
+        _setTokenURI(newItemId, tokenURI);
 
         return newItemId;
     }
 
-    function update(uint256 tokenId, string memory customFieldsURI) public {
+    function updateMetadata(uint256 tokenId, string memory newTokenURI) public {
         require(ownerOf(tokenId) == msg.sender, "ERC721: caller is not the owner");
 
-        _tokenMetadata[tokenId].customFieldsURI = customFieldsURI;
+        _setTokenURI(tokenId, newTokenURI);
     }
 
     function totalSupply() public view returns (uint256) {
