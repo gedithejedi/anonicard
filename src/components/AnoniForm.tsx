d519693b14c226aceedb28792961cbaa841117e6
“DaeunYoon” <daa3230@gmail.com>
2023-06-25 01:36:16 -0400

    working on

--- src/components/AnoniForm.tsx
+++ src/components/AnoniForm.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {
   Path,
   useForm,
@@ -6,9 +6,25 @@ import {
   SubmitHandler,
   FieldError,
 } from 'react-hook-form'
+
+import {
+  useContractRead,
+  useAccount,
+  usePrepareContractWrite,
+  useContractWrite,
+  useWaitForTransaction,
+} from 'wagmi'
+import { polygon } from 'wagmi/chains'
+import { NFTStorage, File } from 'nft.storage'
+
+import Anonicard from '~/abi/Anonicard.json'
+import nftConfig from '~/nftConfig.json'
+import Lit from '~/Lit'
+
+import { useToast } from '@chakra-ui/react'
 import IOriginalNft from '~/types'
 import Button from '~/components/Common/Button'
-import QrReader from 'react-qr-scanner'
+import QrScanner from '~/components/Common/QrScanner'
 
 const STORAGE_API_KEY = process.env.NEXT_PUBLIC_STORAGE_API_KEY
 
@@ -71,13 +87,123 @@ const AnoniForm: React.FC<Props> = ({ defaultNft, onSuccess }) => {
     defaultValues: defaultNft,
   })
 
-  const onSubmit: SubmitHandler<IFormValues> = (data) => {
-    console.log(data)
+  const [toAddress, setToAddress] = useState()
+  const [uris, setUris] = useState<string[] | undefined>()
+  const [defaultImage, setDefaultImage] = useState<File>()
+  const [isMinting, setIsMinting] = useState<boolean>(false)
+
+  // TODO: replace it with Nouns image
+  const loadDefaultImage = async () => {
+    const res = await fetch('/CAT.jpg')
+    if (!res.ok) {
+      throw new Error('Failed to fetch data')
+    }
+    setDefaultImage(
+      new File([await res.blob()], 'defaultImage.jpg', { type: 'image/jpeg' })
+    )
   }
 
-  return (
+  const onSubmit: SubmitHandler<IFormValues> = (data) => {
+    setIsMinting(true)
+    try {
+      const data = {
+        ...data,
+        ...defaultNft,
+      }
+      await storeAsset(data)
+    } catch (e) {
+      console.error(`Minting failed! ${e}`)
+      toast({
+        title: 'Anonycard minting failed.',
+        description: `Your Anonycard could not be minted! ${e}`,
+        status: 'error',
+        duration: 6000,
+        isClosable: true,
+      })
+      setIsMinting(false)
+    }
+  }
+
+  const { refetch } = useContractRead({
+    address: nftConfig.anonicard.address as `0x${string}`,
+    abi: Anonicard.abi,
+    functionName: 'totalSupply',
+  })
+
+  const {
+    config,
+    error: prepareError,
+    isError: isPrepareError,
+  } = usePrepareContractWrite({
+    address: nftConfig.anonicard.address as `0x${string}`,
+    abi: Anonicard.abi,
+    functionName: 'mint',
+    args: [uris[0], uris[1]],
+    chainId: polygon.id,
+    enabled: Boolean(uris),
+  })
+
+  const { data, error, isError, write } = useContractWrite(config)
+  const { isLoading, isSuccess } = useWaitForTransaction({
+    hash: data?.hash,
+  })
+
+  async function storeAsset(formData: IFormValues) {
+    if (!STORAGE_API_KEY) {
+      console.error('Required API Key has not been provided.')
+      throw Error('This method cannot be executed.')
+    }
+
+    const client = new NFTStorage({
+      token: STORAGE_API_KEY,
+    })
+
+    const { data: nftTotalSupply } = await refetch()
+    const encryptedInformation = await Lit.encryptObject(
+      nftName,
+      {
+        walletAddress: formData['Wallet Address']
+        fullName: formData['Full Name'],
+        discordName: formData['Discord Handle'],
+        job: formData['Job'],
+        introduction: formData['Introduction'],
+      },
+      String(Number(nftTotalSupply) + 1)
+    )
+
+    const { encryptedFile, encryptedSymmetricKey: encryptedFileSymmetricKey } =
+      await Lit.encryptFile(
+        nftName,
+        formData['Profile Image']?.[0],
+        String(Number(nftTotalSupply) + 1)
+      )
+
+    // TODO: Encrypt image once we get getting NFT part done.
+    const metadata = await client.store({
+      name: nftName,
+      description: nftDescription,
+      encryptedString: encryptedInformation?.encryptedString,
+      encryptedStringSymmetricKey: encryptedInformation?.encryptedSymmetricKey,
+      image: defaultImage as File,
+      encryptedImage: encryptedFile,
+      encryptedFileSymmetricKey,
+    })
+
+    setUri(metadata.url)
+    console.log('Metadata stored on Filecoin and IPFS with URL:', metadata.url)
+  }
+
+  const onReadQrCode = (data) => {
+    setToAddress(data)
+  }
+  return toAddress ? (
     <form onSubmit={handleSubmit(onSubmit)}>
       <div className="flex flex-col gap-y-2">
+        <p className="pb-2 text-sm">send to: {toAddress}</p>
+        <p className="pb-2 text-sm">
+          send from: {defaultNft['Wallet Address']}
+        </p>
+
         <label className="flex flex-col">
           Profile Image
           <input
@@ -102,6 +228,8 @@ const AnoniForm: React.FC<Props> = ({ defaultNft, onSuccess }) => {
         </div>
       </div>
     </form>
+  ) : (
+    <QrScanner onReadQrCode={onReadQrCode} />
   )
 }
 
