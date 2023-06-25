46d25f125078c2222e24d62e1399a443ec2eb291
“DaeunYoon” <daa3230@gmail.com>
2023-06-25 04:55:05 -0400

    update anoni create

---
+++ src/components/Anoni.tsx
@@ -0,0 +1,106 @@
+import { useState, useEffect, useCallback } from 'react'
+import React from 'react'
+import { useDisclosure } from '@chakra-ui/react'
+
+import { useAccount } from 'wagmi'
+
+import Box from '~/components/Common/Box'
+import Button from '~/components/Common/Button'
+import Modal from '~/components/Common/Modal'
+import useUserOwnedNfts from '~/hooks/useUserOwnedNfts'
+
+export interface AnoniNFT {
+  tokenId: string
+  profileImage?: Blob
+  profileImageUrl?: string
+  fullName: string
+  discordName: string
+  job: string
+  introduction: string
+  occasion: string
+  memo: string
+}
+
+const Anony: React.FC<Props> = () => {
+  const [error, setError] = useState<string | undefined>()
+  const { isOpen, onOpen, onClose } = useDisclosure()
+
+  const { airstackFetch, nfts, loading } =
+    useUserOwnedNfts<AnoniNFT>('anonicard')
+
+  useEffect(() => {
+    airstackFetch()
+  }, [])
+
+  const { address } = useAccount()
+
+  const onSuccess = () => {
+    setError(undefined)
+    onOpen()
+  }
+
+  const onFormSucess = () => {
+    onClose()
+    airstackFetch()
+  }
+
+  return (
+    <>
+      <Box classes="bg-beige" title="My Anoni">
+        {loading ? (
+          'Loading ... '
+        ) : nfts.length ? (
+          <>
+            {/* eslint-disable-next-line @next/next/no-img-element */}
+            <img src={nfts[0].profileImageUrl} alt="profile" />
+            <table>
+              <tbody className="divide-y-2 divide-solid div-">
+                <tr>
+                  <th>Full Name</th>
+                  <td>{nfts[0].fullName}</td>
+                </tr>
+                <tr>
+                  <th>Discord Name</th>
+                  <td>{nfts[0].discordName}</td>
+                </tr>
+                <tr>
+                  <th>Job</th>
+                  <td>{nfts[0].job}</td>
+                </tr>
+                <tr>
+                  <th>Introduction</th>
+                  <td>{nfts[0].introduction}</td>
+                </tr>
+                <tr>
+                  <th>Occasion</th>
+                  <td>{nfts[0].occasion}</td>
+                </tr>
+                <tr>
+                  <th>Memo</th>
+                  <td>{nfts[0].memo}</td>
+                </tr>
+              </tbody>
+            </table>
+            <div className="flex justify-between mt-3">
+              <button
+                className="font-bold bg-black text-white transition-shadow duration-300 py-2 px-4 border-2 border-black hover:text-black hover:bg-white"
+                onClick={() => console.log('annoicard')}
+              >
+                Edit Anonicard
+              </button>
+            </div>
+          </>
+        ) : (
+          <>
+            <p className="mb-10">
+              You do not own an Anoni yet! Start to connect with Frens by
+              exchanging Anonicard!
+            </p>
+          </>
+        )}
+      </Box>
+    </>
+  )
+}
+
+export default Anony
